9baea9ad3f89eef7cacdd9b1670fe0fefa2f32bc
Richard Melo <richard.meloneto@gmail.com>
2023-11-27 23:26:45 -0300

    arrumando cabeçalho

--- newCard.php
+++ newCard.php
@@ -14,14 +14,14 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
-                <a class="navbar-brand" href="index.php">Crud-tests</a>
+                <a class="navbar-brand" href="#">Crud-tests</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Grafícos</a>
+                            <a class="nav-link" href="graph.php">Gráficos</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="newCard.php">Cadastro</a>
